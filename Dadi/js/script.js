6c1787b454e2f5cfbaec97b928c3fcd1a00b2829
Robertarezza <roberta.rezza@gmail.com>
2024-03-28 18:26:04 +0100

    fix: correction for loop in project mail

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -6,23 +6,33 @@ const sendBtnElem = document.getElementById("btn-utente"); // object | null
 console.log(sendBtnElem);
 
 
-sendBtnElem.addEventListener("click", function() {
+sendBtnElem.addEventListener("click", function () {
 
-const numberPc=Math.floor(Math.random() * 6) + 1 ; //number
-console.log(numberPc, typeof numberPc);
-document.getElementById("num-pc").innerHTML= `Questo è il numero del pc :  ${numberPc}` ;
+    const numberPc = Math.floor(Math.random() * 6) + 1; //number
+    console.log(numberPc, typeof numberPc);
+    document.getElementById("num-pc").innerHTML = `Questo è il numero del pc :  ${numberPc}`;
 
-const numberUt=Math.floor(Math.random() * 6) + 1 ; //number
-console.log(numberUt, typeof numberUt);
-document.getElementById("num-utente").innerHTML= `Questo è il tuo numero :  ${numberUt}` ;
+    const numberUt = Math.floor(Math.random() * 6) + 1; //number
+    console.log(numberUt, typeof numberUt);
+    document.getElementById("num-utente").innerHTML = `Questo è il tuo numero :  ${numberUt}`;
+
+    let result = "";
+    if (numberPc == numberUt) {
+        result = 0;
+
+    } else if (numberPc < numberUt) {
+        result = 1
+
+    } else if (numberPc > numberUt) {
+        result = 2;
+    }
 
 
-if (numberPc == numberUt){
-    document.getElementById("msg").innerHTML= "Riprova";
-} else if (numberPc < numberUt) {
-    document.getElementById("msg").innerHTML= "Complimenti, hai vinto!!!";
-}else if (numberPc > numberUt) {
-    document.getElementById("msg").innerHTML= "Hai perso, ritenta sarai più fortunato";
-}
+    if (result == 0) {
+        document.getElementById("msg").innerHTML = "Riprova";
+    } else if (result == 1) {
+        document.getElementById("msg").innerHTML = "Complimenti, hai vinto!!!";
+    } else if (result == 2) {
+        document.getElementById("msg").innerHTML = "Hai perso, ritenta sarai più fortunato";
+    }
 })
-
--- Mail/js/script.js
+++ Mail/js/script.js
@@ -9,31 +9,52 @@ console.log(sendBtnElem);
 //const userMail= prompt("Qual'è la tua mail?"); //string | null
 //console.log(userMail, typeof userMail);
 
-//evento
-sendBtnElem.addEventListener("click", function() {
-
-//lista arrey    
 const mail = ["pippo@live.it", "peppe@gmail.com", "roberta.rezza@gmail.com"]
 
-//input
-const mailInputElem = document.getElementById("user_mail").value; // string
-console.log(mailInputElem);
+
+//evento
+sendBtnElem.addEventListener("click", function () {
+
+    //lista arrey    
 
 
-if (isNaN(mailInputElem) === true) {
+    //input
+    const mailInputElem = document.getElementById("user_mail").value; // string
+    console.log(mailInputElem);
 
-//ciclo
- for (let i= 0; i < mail.length; i++){
-    //console.log(i);
-  //condizioni
-    if(mailInputElem == mail[i]){
-    //stampa
-        document.getElementById("msg").innerHTML= "E' autorizzato ad accedere, il sito è in manutenzione";
-    }else if (mailInputElem !== mail[i])
-    //stampa
-    document.getElementById("msg").innerHTML= "Siamo spiacenti, la mail non risulta registrata";
-}
-} else{
-    alert("dati errati")
-}
+let trovato= false
+
+    if (isNaN(mailInputElem) === true) {
+
+        //ciclo
+        for (let i = 0; i < mail.length; i++) {
+            //console.log(i);
+            //condizioni
+            if (mailInputElem == mail[i]) {
+                trovato = true
+                //stampa 
+            }
+        }
+        
+        if(trovato == true){
+            document.getElementById("msg").innerHTML = "E' autorizzato ad accedere, il sito è in manutenzione";
+        }else {
+            document.getElementById("msg").innerHTML = "Non autorizzato";
+        }
+        
+    } else {
+        alert("dati errati")
+    }
+
+
 })
+
+//leggo ad uno ad uno tutta la lista
+    //se il nome che sto leggendo è uguale, lo ricordo 
+
+//se è presente il nome nella lista 
+    //lo faccio entrare
+//altrimenti
+    //non lo faccio entrare
+
+    //facciamo diventare vera la condizione del for, quando ci serve
